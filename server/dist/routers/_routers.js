a6652ec55e14f39316481ca728276663bf6e521e
artndev <97910780+artndev@users.noreply.github.com>
2025-05-12 19:39:39 +0300

    .

--- server/dist/routers/_routers.js
+++
@@ -1,4 +0,0 @@
-import githubRouter from './github_router.js';
-import googleRouter from './google_router.js';
-import localRouter from './local_router.js';
-export { githubRouter, googleRouter, localRouter };
