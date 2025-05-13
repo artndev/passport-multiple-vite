ee708dfa1109f13ab97ab985adab74a221989067
artndev <97910780+artndev@users.noreply.github.com>
2025-05-14 01:09:41 +0300

    .

--- server/src/routers/local_router.ts
+++ server/src/routers/local_router.ts
@@ -1,13 +1,30 @@
 import express from 'express'
 import passport from 'passport'
+import config from '../config.json' with { type: 'json' }
 
 const router = express.Router()
 
-router.post('/login', passport.authenticate('local-login'), (req, res) => {
-  res.status(200).json({
-    message: 'You have successfully logged in',
-    answer: req.user,
-  })
+router.post('/login', (req, res, next) => {
+  return passport.authenticate(
+    'local-login',
+    {
+      failureRedirect: `${config.CLIENT_URL}/fallback`,
+    },
+    (
+      err: Error | null | unknown,
+      user: Express.User | false,
+      _info: object
+    ) => {
+      if (err) return res.redirect(`${config.CLIENT_URL}/register`)
+      if (!user) return res.redirect(`${config.CLIENT_URL}/fallback`)
+
+      req.logIn(user, err2 => {
+        if (err2) return res.redirect(`${config.CLIENT_URL}/fallback`)
+
+        return res.redirect(`${config.CLIENT_URL}`)
+      })
+    }
+  )(req, res, next)
 })
 
 router.post(
