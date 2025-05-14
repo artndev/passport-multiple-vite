1a114f094ee1540c169b73de4e580908f226fca8
artndev <97910780+artndev@users.noreply.github.com>
2025-05-14 19:40:10 +0300

    .

--- server/src/routers/local_router.ts
+++ server/src/routers/local_router.ts
@@ -5,38 +5,20 @@ import config from '../config.json' with { type: 'json' }
 const router = express.Router()
 
 // IT DIDN'T HELP ME!
-router.post('/login', (req, res, next) => {
-  return passport.authenticate(
-    'local-login',
-    {
-      failureRedirect: `${config.CLIENT_URL}/fallback`,
-    },
-    (
-      err: Error | null | unknown,
-      user: Express.User | false,
-      _info: object
-    ) => {
-      if (err) return res.redirect(`${config.CLIENT_URL}/register`)
-      if (!user) return res.redirect(`${config.CLIENT_URL}/fallback`)
-
-      req.logIn(user, err2 => {
-        if (err2) return res.redirect(`${config.CLIENT_URL}/fallback`)
-
-        return res.redirect(`${config.CLIENT_URL}`)
-      })
-    }
-  )(req, res, next)
-})
+router.post(
+  '/login',
+  passport.authenticate('local-login', {
+    failureRedirect: `${config.CLIENT_URL}/fallback`,
+    successReturnToOrRedirect: `${config.CLIENT_URL}`,
+  })
+)
 
 router.post(
   '/register',
-  passport.authenticate('local-register'),
-  (req, res) => {
-    res.status(200).json({
-      message: 'You have successfully registered',
-      answer: req.user,
-    })
-  }
+  passport.authenticate('local-register', {
+    failureRedirect: `${config.CLIENT_URL}/fallback`,
+    successReturnToOrRedirect: `${config.CLIENT_URL}`,
+  })
 )
 
 export default router
