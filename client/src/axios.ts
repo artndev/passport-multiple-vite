65c40d88155564d3bb3f93412666d272e2c4350e
artndev <97910780+artndev@users.noreply.github.com>
2025-05-15 14:02:21 +0300

    .

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -4,6 +4,9 @@ import config from './config.json'
 const instance = axios.create({
   baseURL: config.SERVER_URL,
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 export default instance
